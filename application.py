4ef9245ace3744a70c675eb3e0da617932d9b59f
Chris <Silber8806@gmail.com>
2019-07-08 18:46:29 -0400

    add error handler

--- application.py
+++ application.py
@@ -64,6 +64,11 @@ def flash_err(message):
     flash(message,'danger')
     return 0
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 @app.route("/")
 def index():
     """ main page of website """
